3fefa86944526e5eaa213ca7343ec912a12730ad
Ego Victor <egovictorc@outlook.com>
2020-09-24 14:58:33 +0100

    (feat) add messagebox to editor_complex

--- editor_complex.py
+++ editor_complex.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import filedialog as fd
+
 
 class Editor():
     def __init__(self, master):
         self.master = master
-        self.master.title("NIIT Editor")
-        self.text_editor = tk.Text(self.master, fg="#fefefe", bg="#555")
+        master.title("NIIT Editor")
+        self.text_editor = tk.Text(master, fg="#fefefe", bg="#555")
         self.text_editor.insert(1.0, "Edit your text here")
         self.text_editor.pack(fill=tk.BOTH, expand=1)
         self.createMenu()
@@ -19,23 +21,45 @@ class Editor():
         edit = tk.Menu(menubar, tearoff=0)
         help_ = tk.Menu(menubar, tearoff=0)
 
-        #file menus
-        file.add_command(label="Open", command=self.open)
-        file.add_command(label="Save", command=self.save)
-        file.add_command(label="Open recent")
-        file.add_command(label="Save as...", command=self.save_as)
-        file.add_command(label="Exit", command=self.master.quit)
+        # file menus
+        file_menu = [
+            {"label": "Open",
+             "command": self.open
+             }, {
+                "label": "Save",
+                "command": self.save
+            }, {
+                "label": "Open recent",
+            }, {
+                "label": "Save as...",
+                "command": self.save_as
+            }, {
+                "label": "Exit",
+                "command": self.exit
+            },
+        ]
+        for menu in file_menu:
+            file.add_command(**menu)
 
-        #edit menus
-        edit.add_command(label="Cut")
-        edit.add_command(label="Copy")
-        edit.add_command(label="Paste")
-        edit.add_command(label="Delete")
-        edit.add_command(label="Find")
+        # edit menus
+        edit_menu = [
+            {"label": "Copy"},
+            {"label": "Cut"},
+            {"label": "Paste"},
+            {"label": "Delete"},
+            {"label": "Find"},
+        ]
+        for menu in edit_menu:
+            edit.add_command(**menu)
 
-        help_.add_command(label="Help")
-        help_.add_command(label="Updates")
-        help_.add_command(label="About")
+        #help menus
+        help_menu = [
+            {"label": "Help"},
+            {"label": "Update"},
+            {"label": "About"},
+        ]
+        for menu in help_menu:
+            help_.add_command(**menu)
 
         menubar.add_cascade(menu=file, label="File")
         menubar.add_cascade(menu=edit, label="Edit")
@@ -45,16 +69,16 @@ class Editor():
 
     def open(self):
         """open file for editing"""
-        filepath = fd.askopenfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
-        )
-        self.open_filepath  = filepath
+        filepath = fd.askopenfilename(title="Select file", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+                                      )
+        self.open_filepath = filepath
         if not filepath:
             return
         with open(filepath, "r") as file:
             text = file.read()
             self.master.title(f"NIIT Editor:- {filepath}")
-            self.text_editor.insert(tk.INSERT, text)
-            print(text)
+            self.text_editor.delete("1.0", tk.END)
+            self.text_editor.insert("1.0", text)
 
     def save(self):
         """save file"""
@@ -63,13 +87,21 @@ class Editor():
             file.write(text)
 
     def save_as(self):
-        """save file"""
+        """save as file"""
         filepath = fd.asksaveasfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
         if not filepath:
             return
         with open(filepath, "w") as file:
             text = self.text_editor.get(1.0, tk.END)
             file.write(text)
+
+    def exit(self):
+        if messagebox.askyesno(title="confirm exit", message="Do you wish to exit?"):
+            self.master.quit()
+        else:
+            print("no exit")
+
+
 
 window = tk.Tk()
 editor = Editor(window)
